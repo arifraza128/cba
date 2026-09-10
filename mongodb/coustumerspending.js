4b7bd8648f6daa4c71d8fae285b2c67a4bfd8d93
Arif Raza <arif3raza@gmail.com>
2026-09-10 21:12:17 +0530

    Add aggregation query for customer spending analysis

--- mongodb/coustumerspending.js
+++ mongodb/coustumerspending.js
@@ -1,1 +1,27 @@
-
+db.orders.aggregate([
+{
+$group: {
+_id: "$customerId",
+totalOrders: {$sum: 1},
+totalAmountSpent: {$sum: "$amount"}
+}
+},
+{
+$match: {
+totalAmountSpent: {$gt: 50000}
+}
+},
+{
+$sort: {
+totalAmountSpent: -1
+}
+},
+{
+$project: {
+_id: 0,
+customerId: "$_id",
+totalOrders: 1,
+totalAmountSpent: 1
+}
+}
+])
