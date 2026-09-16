b13e3ca095db8d38961f710b287f75db62c909cd
Arif Raza <arif3raza@gmail.com>
2026-09-16 21:42:12 +0530

    Insert sample data into Students table

--- sql/collegedb.sql
+++ sql/collegedb.sql
@@ -6,3 +6,10 @@ CREATE TABLE Students (
     Department VARCHAR(50),
     Marks INT
 );
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES
+(1, 'Arif', 21, 'CSE', 85),
+(2, 'Rahul', 22, 'ECE', 78),
+(3, 'Aman', 20, 'CSE', 92),
+(4, 'Priya', 21, 'ISE', 88),
+(5, 'Sneha', 22, 'CSE', 74);
